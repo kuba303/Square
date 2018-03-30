05a94ca06409e181cb11743c45dc3b7342b1b438
Kuba <kuba81s@o2.pl>
2018-03-30 10:14:10 +0100

    html markup

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+
+	<nav>
+		<div class="logo"><a href="#">TITLE</a></div>
+
+		<div class="nav-list">
+			<ol>
+				<li><a href="#">WORK</a></li>
+				<li><a href="#">BLOG</a></li>
+				<li><a href="#">ABOUT</a></li>
+			</ol>
+		</div>
+	</nav>
+
+	<main class="main">
+		
+	</main>
+
+	<footer class="footer">
+			
+	</footer>
+
+
+
+	
+
+</body>
+</html>
